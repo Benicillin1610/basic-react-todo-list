3b7e67536b1fa7500d48bd462dce0c5947a58fdb
Benjamin Hung <79796028+Benicillin1610@users.noreply.github.com>
2022-01-11 16:10:32 +1100

    Add files via upload

---
+++ build/precache-manifest.d4168f52d5e5ea421ac26df3db75b651.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c4878d0400439549983",
+    "url": "/static/css/main.751be1f5.chunk.css"
+  },
+  {
+    "revision": "2c4878d0400439549983",
+    "url": "/static/js/main.97553395.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "84d6c2520a5215e9e63c",
+    "url": "/static/js/2.286e2e93.chunk.js"
+  },
+  {
+    "revision": "6f6fe54ff6bf546650fe408245acbeb4",
+    "url": "/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,21 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.d4168f52d5e5ea421ac26df3db75b651.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
